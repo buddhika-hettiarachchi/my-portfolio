9819e0b9738e61e684916110b08a4dba35735808
Buddhika <webspeaky@gmail.com>
2022-08-29 00:12:18 +0530

    add hero section without color and design

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,14 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+import Hero from "../components/Hero"
+import TechnologiesCarousel from "../components/TechnologiesCarousel"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-    </div>
+    <Hero></Hero>
+    <TechnologiesCarousel></TechnologiesCarousel>
   </Layout>
 )
 
