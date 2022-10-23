33f990c53d30abb2d529ef0dc7c5229cc164cfaa
buddhika <buddhika.hettiarachchi@kognitiv.com>
2022-10-23 15:31:49 +0530

    change scroll bar color and width, avoid FOUC

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,7 @@
  */
 
 // You can delete this file if you're not using it
-import './src/styles/global.css'
+import "./src/styles/global.css"
 import "@fontsource/inter"
 import "@fontsource/comfortaa"
 import "@fontsource/poppins"
--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,8 @@ module.exports = {
     },
     'gatsby-plugin-postcss',
     `gatsby-plugin-smoothscroll`,
-    `gatsby-plugin-preload-fonts`
+    `gatsby-plugin-preload-fonts`,
+    `gatsby-plugin-fix-fouc`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
