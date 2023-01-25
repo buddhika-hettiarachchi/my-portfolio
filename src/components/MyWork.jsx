309c1fb99564808139adb0be57517fe22112bc7c
buddhika <buddhika.hettiarachchi@kognitiv.com>
2023-01-25 23:57:40 +0530

    update stying in my work component

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { StaticImage } from "gatsby-plugin-image"
 
+import hashTag from "../images/icons/hashtag.svg"
+import link from "../images/icons/link.svg"
+import website from "../images/icons/website.svg"
+import github from "../images/icons/github.svg"
+
 const MyWork = () => {
     return (
         <div id="my-work" className='bg-secondary text-primary-text'>
@@ -13,24 +18,22 @@ const MyWork = () => {
                     <div className='w-full h-[600px] max-w-lg rounded flex-col '>
                         <div className='relative bg-gradient-to-b from-[#b8cff1] to-[#dfe9f9] rounded-t h-4/6'>
                             <div className='absolute z-10 inset-0 top-16 left-16 rounded-t-lg overflow-hidden'>
-                                <StaticImage quality='100' style={{height: "100%"}} objectFit='cover' objectPosition='top left' src="../images/mopicClub.webp" alt="mopicClub" placeholder="blurred"/>
+                                <StaticImage quality='100' style={{ height: "100%" }} objectFit='cover' objectPosition='top left' src="../images/mopicClub.webp" alt="mopicClub" placeholder="blurred" />
                             </div>
                         </div>
                         <div className='h-2/6 flex flex-col gap-3'>
-                            <div className='flex gap-8 mt-5 text-sm'>
-                                <div className='text-sm'>
-                                    <span className='inline-block'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 mr-1">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
-                                    </svg>
-                                    </span>
+                            <div className='flex gap-3 sm:gap-8 mt-5 mb-3'>
+                                <div className='text-xs lg:text-sm opacity-60'>
+                                    <img className='inline  w-3 h-3 mr-1' src={hashTag} alt="#" />
                                     Angular
                                 </div>
-                                <div className='text-sm'>
-                                    <span className='inline-block'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 mr-1">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
-                                    </svg>
-                                    </span>
+                                <div className='text-xs lg:text-sm opacity-60'>
+                                    <img className='inline opacity-80 w-3 h-3 mr-1' src={hashTag} alt="#" />
                                     The Movie DB API
+                                </div>
+                                <div className='text-xs lg:text-sm opacity-60'>
+                                    <img className='inline opacity-80 w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                    Firebase
                                 </div>
                             </div>
                             <div className='text-xl font-bold'>
@@ -39,13 +42,12 @@ const MyWork = () => {
                             <div className='text-secondary-text'>
                                 Check out how I helped develop the front-end of a website that analyzes marketing and sales data in BigQuery
                             </div>
-                            <div className='text-[blue]'>
-                                <a href="https://moviedb-90211.web.app/" target="_blank">View case study
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-[blue] inline-block pl-2
-                                 ">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                                    </svg>
-                                    <div className='w-[9.8rem] h-[0.1rem] bg-[blue]'></div>
+                            <div className='flex gap-4'>
+                                <a href="https://moviedb-90211.web.app/" target="_blank">
+                                    <img className='w-5 h-5' src={link} alt="web-link" />
+                                </a>
+                                <a href="https://moviedb-90211.web.app/" target="_blank">
+                                    <img className='w-5 h-5' src={github} alt="web-link" />
                                 </a>
                             </div>
                         </div>
@@ -56,26 +58,41 @@ const MyWork = () => {
                         <div className=' w-full h-full lg:mt-72'>
                             <div className='relative bg-gradient-to-b from-[#97dfff] to-[#c4edff] rounded-t h-4/6'>
                                 <div className='absolute z-10 inset-0 top-16 left-16 rounded-t-lg overflow-hidden '>
-                                    <StaticImage quality='100' style={{height: "100%"}} objectFit='cover'  objectPosition='top left' src="../images/chefscookeryclub.webp" alt="chefscookeryclub" placeholder="blurred"/>
+                                    <StaticImage quality='100' style={{ height: "100%" }} objectFit='cover' objectPosition='top left' src="../images/chefscookeryclub.webp" alt="chefscookeryclub" placeholder="blurred" />
                                 </div>
                             </div>
                             <div className='h-2/6 flex flex-col gap-3'>
-                                <div className='flex gap-8 mt-5'>
-                                    <div>
-                                        Angular
+                                <div className='flex gap-3 sm:gap-8 mt-5 mb-3'>
+                                    <div className='text-xs lg:text-sm opacity-60'>
+                                        <img className='inline  w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                        React
                                     </div>
-                                    <div>
-                                        Rest
+                                    <div className='text-xs lg:text-sm opacity-60'>
+                                        <img className='inline opacity-80 w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                        Gatsby
+                                    </div>
+                                    <div className='text-xs lg:text-sm opacity-60'>
+                                        <img className='inline opacity-80 w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                        Flotiq( CMS )
+                                    </div>
+                                    <div className='text-xs lg:text-sm opacity-60'>
+                                        <img className='inline opacity-80 w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                        Firebase
                                     </div>
                                 </div>
                                 <div className='text-xl font-bold'>
-                                    Query.Recipes
+                                    Chefs' Cookery Club
                                 </div>
                                 <div>
                                     Check out how I helped develop the front-end of a website that analyzes marketing and sales data in BigQuery
                                 </div>
-                                <div>
-                                    <a href="#">View case study</a>
+                                <div className='flex gap-4 relative z-50'>
+                                    <a href="https://gatsby-chefs-recipes.web.app/" target="_blank">
+                                        <img className='w-5 h-5' src={link} alt="web-link" />
+                                    </a>
+                                    <a href="#" target="_blank">
+                                        <img className='w-5 h-5' src={github} alt="web-link" />
+                                    </a>
                                 </div>
                             </div>
                             <div>
@@ -87,26 +104,41 @@ const MyWork = () => {
                     <div className='w-full h-[600px] max-w-lg rounded flex-col'>
                         <div className='relative bg-gradient-to-b from-[#ffbd5f] to-[#ffd395] rounded-t h-4/6'>
                             <div className='absolute z-10 inset-0 top-16 left-16 rounded-t-lg overflow-hidden'>
-                                <StaticImage quality='100' style={{height: "100%"}} objectFit='cover'  objectPosition='top left' src="../images/bidspell.webp" alt="bidspell" placeholder="blurred"/>
+                                <StaticImage quality='100' style={{ height: "100%" }} objectFit='cover' objectPosition='top left' src="../images/bidspell.webp" alt="bidspell" placeholder="blurred" />
                             </div>
                         </div>
                         <div className='h-2/6 flex flex-col gap-3'>
-                            <div className='flex gap-8 mt-5'>
-                                <div>
-                                    Angular
+                            <div className='flex gap-3 sm:gap-8 mt-5 mb-3'>
+                                <div className='text-xs lg:text-sm opacity-60'>
+                                    <img className='inline  w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                    React
                                 </div>
-                                <div>
-                                    Rest
+                                <div className='text-xs lg:text-sm opacity-60'>
+                                    <img className='inline opacity-80 w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                    Gatsby
+                                </div>
+                                <div className='text-xs lg:text-sm opacity-60'>
+                                    <img className='inline opacity-80 w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                    HubSpot
+                                </div>
+                                <div className='text-xs lg:text-sm opacity-60'>
+                                    <img className='inline opacity-80 w-3 h-3 mr-1' src={hashTag} alt="#" />
+                                    chrisp( Live Chat )
                                 </div>
                             </div>
                             <div className='text-xl font-bold'>
-                                Query.Recipes
+                                Bidspell
                             </div>
                             <div>
                                 Check out how I helped develop the front-end of a website that analyzes marketing and sales data in BigQuery
                             </div>
-                            <div>
-                                <a href="#">View case study</a>
+                            <div className='flex gap-4'>
+                                <a href="https://bidspell.com/" target="_blank">
+                                    <img className='w-5 h-5' src={link} alt="web-link" />
+                                </a>
+                                <a href="https://moviedb-90211.web.app/" target="_blank">
+                                    <img className='w-5 h-5' src={github} alt="web-link" />
+                                </a>
                             </div>
                         </div>
                         <div>
